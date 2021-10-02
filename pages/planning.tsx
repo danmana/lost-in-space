b4f5720f76fc397130ab3a963709878967a874d1
Alexandru Pirvu <pirvu.alexandru@outlook.com>
2021-10-02 21:22:04 +0300

    Calculate total resource weight and price

--- pages/planning.tsx
+++ pages/planning.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+import planningStyles from "../styles/Planning.module.scss";
 import Link from "next/link"
 import React, { useState } from "react";
 import { Warehouse } from "../common/model/warehouse.model";
@@ -21,29 +22,46 @@ const resources: { [key: string]: Resource } = {
 const Planning = () => {
   const [warehouse, setWarehouse] = useState(emptyWarehouse);
 
+  const totalWeight = Object.entries(warehouse.resources).reduce(((previousValue, [_, resourceEntry]) => {
+    const currentWeight = resourceEntry.quantity * resourceEntry.resource.weight;
+    return previousValue + currentWeight;
+  }), 0)
+  const totalPrice = Object.entries(warehouse.resources).reduce(((previousValue, [_, resourceEntry]) => {
+    const currentPrice = resourceEntry.quantity * resourceEntry.resource.price;
+    return previousValue + currentPrice;
+  }), 0)
+
   const changeResourceQuantity = (key: string, quantity: number) => {
     warehouse.resources[key] = { resource: resources[key], quantity: quantity };
-    setWarehouse(warehouse);
+    setWarehouse({ ...warehouse });
   }
 
-  const resourcePickers = Object.entries(resources).map(([key, value], index) => {
+  const resourceContainers = Object.entries(resources).map(([key, value], index) => {
     return (
-      <div key={key}>
-        <label>{value.type}</label>
-        <input type="text"
-               value={warehouse.resources[key]?.quantity}
+      <div className={planningStyles['resource']} key={key}>
+        <label className={planningStyles.label}>{value.type}</label>
+        <input type="number"
                onChange={(event) => changeResourceQuantity(key, Number.parseInt(event.target.value))}/>
+        <span>t</span>
+        <span className={planningStyles.quantity}>{(warehouse.resources[key]?.quantity | 0) * resources[key].price}$</span>
       </div>
     );
   })
 
-  console.log(warehouse)
-
   return (
     <div className={styles.container}>
-      <main>
+      <main className={planningStyles.container}>
         <h1>Plan your resources so that the space crew can reach mars!</h1>
-        {resourcePickers}
+        <div className={planningStyles['resources-container']}>
+          {resourceContainers}
+        </div>
+
+        <div className={planningStyles.totals}>
+          <span>Total: </span>
+          <span>{totalWeight}kg</span>
+          <span>{totalPrice}$</span>
+        </div>
+
         <Link href={"/execution"}>
           Let's see if you got everything right
         </Link>

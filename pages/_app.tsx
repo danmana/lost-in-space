75b0038232ae02cef681ad8df712d766373162b7
Dana Ciubancan <dana.ciubancan@fxbits.io>
2021-10-02 19:21:03 +0300

    feat: add font and information content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <title>Lost In Space</title>
           <meta name="description" content="Lost in space - an educational game that simulates a ride frm earth to mars" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
         </Head>
 
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,69 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import indexStyles from '../styles/Index.module.scss'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Image from 'next/image'
+import Rocket from '../public/rocket.webp';
+import Astro from '../public/happy_astronaut.webp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const Introduction = {
+  messages: [
+    {
+      content: "Welcome to <i><b>Lost in Space</b></i>. Here is the first message from our introduction. We hope you will like our game. Enjoy!",
+      resource: Rocket
+    },
+    {
+      content: "Here you can meet the Happy astronaut. Over and over again. What is your name? Here you can meet the Happy astronaut. Over and over again. What is your name?",
+      resource: Astro,
+      inputs: [{
+        name: "username",
+        placeholder: "Enter your name"
+      }]
+    }
+  ]
+};
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [currentMessage, setCurrentMessage] = useState(0);
+  const messages = Introduction.messages;
+
+  const goNext = () => {
+    const isLast = currentMessage + 1 > messages.length - 1;
+    if (isLast) {
+      router.push('/planning');
+      return;
+    }
+    setCurrentMessage(currentMessage + 1);
+  }
+
+  const goBack = () => {
+    if (currentMessage > 0) {
+      setCurrentMessage(currentMessage - 1);
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Hello! Look at the space wonders! Wanna go out for a ride?</h1>
-          <Link href={"/planning"}>
-              Let's plan this trip!
-          </Link>
+      <main className={indexStyles.container}>
+        <section className={indexStyles.messages}>
+          <div
+            className={indexStyles.message}
+            dangerouslySetInnerHTML={{__html: messages[currentMessage].content}}
+          >
+          </div>
+          <div className={indexStyles.buttons}>
+            {currentMessage > 0 && <FontAwesomeIcon icon={faChevronLeft} onClick={goBack} />}
+            <FontAwesomeIcon icon={faChevronRight} onClick={goNext} />
+            {(currentMessage === messages.length - 1) && <div>Start here</div>}
+          </div>
+        </section>
+        <section className={indexStyles.resources}>
+          <Image src={messages[currentMessage].resource}/>
+        </section>
       </main>
     </div>
   )

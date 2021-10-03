cecadd656fb9014445a28c3fc6da08645c7de34c
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2021-10-03 12:05:30 +0300

    feat: add more events

--- common/model/event.model.ts
+++ common/model/event.model.ts
@@ -291,24 +291,36 @@ export const events: Event[] = [
     // Solution: Who needs Earth anyway?: Spacecraft Health -10, Hapiness -5, No Communication
     // Solution: Fix the antenna: Spare parts -20
   },
+  // TODO: this might be a good idea to add
   // {
   //   name: "Wrong trajectory",
   //   content: "During launch one of the engines underperformed and now the spacecraft is headed on the wrong trajectory",
-  //   chance: 0.6, // high risk on first day
+  //   chance: 0.5, // high risk on first day
   //   chanceIncrement: -0.1, // risk decreases with time
+  //   effects: { health: -100, hapiness: -100 },
+  //   solutions: [
+  //     { text: 'Ignore it and Die', effects: { health: -100, hapiness: -100 } },
+  //     { text: 'Fix the trajectory', effects: { fuel: -100 } },
+  //   ]
   //   // Effect: Health -100, Hapiness -100 // Die
   //   // Solution: Fix the trajectory: Fuel -100
   // },
-  // {
-  //   name: "Hull breach",
-  //   content: "A micrometeorite caused a small puncture in the hull. You are loosing air pressure.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Spacecraft Health -10, Health -30, Hapiness -30, Oxygen -30
-  //   // Solution: It's just a small hole we can live with it: Spacecraft Health -10, Health -30, Hapiness -30, Oxygen -30
-  //   // Solution: Use reserve tank: Spacecraft Health -10, Hapiness -10, Oxygen -100
-  //   // Solution: Patch the hole: Spare parts -20
-  // },
+  {
+    name: "Hull breach",
+    content: "A micrometeorite caused a small puncture in the hull. You are loosing air pressure.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { aircraft: -10, health: -30, hapiness: -30, oxygen: -30 },
+    solutions: [
+      { text: 'It\'s just a small hole we can live with it', effects: { aircraft: -10, health: -30, hapiness: -30, oxygen: -30 } },
+      { text: 'Use reserve tank', effects: {  aircraft: -10, hapiness: -10, oxygen: -100 } },
+      { text: 'Patch the hole', effects: { equipment: -20 } },
+    ]
+    // Effect: Spacecraft Health -10, Health -30, Hapiness -30, Oxygen -30
+    // Solution: It's just a small hole we can live with it: Spacecraft Health -10, Health -30, Hapiness -30, Oxygen -30
+    // Solution: Use reserve tank: Spacecraft Health -10, Hapiness -10, Oxygen -100
+    // Solution: Patch the hole: Spare parts -20
+  },
   // {
   //   // TODO: this is only for landing on Mars!
   //   name: "Mars dust storm",
@@ -319,83 +331,128 @@ export const events: Event[] = [
   //   // Solution: Wait for the storm to pass
   //   // Solution: Risk the landing (50% chance of death)
   // },
-  // {
-  //   name: "I like this song",
-  //   content: "One of your crew was playing a song that you like. Sharing this made the bond between you stronger.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Hapiness +5
-  // },
-  // {
-  //   name: "Can you please turn that down?",
-  //   content: "One of your crew was playing a song that you hate on repeat. You told them to skip the song, but they ignored you and turned the volume up",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Hapiness -5
-  //   // Solution: Yell at them to turn it down: Hapiness -5
-  //   // Solution: Play your own music louder: Hapiness -4, Music -2
-  // },
-  // {
-  //   name: "Water leak",
-  //   content: "One of the water pipes burst and you have a leak in the crew module.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Spacecraft Health -10, Hapiness -5, Water -30
-  //   // Solution: Close the valve: Spacecraft Health -10, Hapiness -5, Water -30
-  //   // Solution: Replace the pipes: Water -20, Spare parts -20
-  // },
-  // {
-  //   name: "Water or Ice",
-  //   content: "Water in the main tank has frozen due to an insulator that fell of during launch.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Spacecraft Health -10, Hapiness -5, Water -30
-  //   // Solution: Ignore it, we still have a reserve: Spacecraft Health -10, Hapiness -5, Water -30
-  //   // Solution: Create a heater and melt the ice: Spare parts -10
-  // },
-  // {
-  //   name: "Movie night",
-  //   content: "One of the astronauts proposes you do a movie night.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Hapiness +5, Movies -2
-  //   // Solution: We have work to do: Hapiness -2
-  //   // Solution: Movie night: Hapiness +5, Movies -2
-  //   // Solution: Bring out the pizza: Hapiness +10, Movies -2, Food -10
-  // },
-  // {
-  //   name: "Green or Gray",
-  //   //https://99designs.com/blog/tips/how-color-impacts-emotions-and-behaviors/
-  //   content: "Seeing green plants and taking care of them can be a great mood booster. The color green can make you feel optimistic and refreshed. When everything around is gray the mood of the crew will be more bland.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Hapiness +5, Movies -2
-  //   // Solution: Let's plant some plants: Hapiness +5, Water -5
-  //   // Solution: We can't waste the water: Hapiness -5
-  // },
-  // {
-  //   name: "Fire",
-  //   // https://www.space.com/13766-international-space-station-flex-fire-research.html
-  //   content: "Fire is a different beast in space than it is on the ground. \nWhen flames burn on Earth, heated gases rise from the fire, drawing oxygen in and pushing combustion products out. In microgravity, hot gases don't rise. So an entirely different process, called molecular diffusion, drives flame behavior.\n" +
-  //     "Space flames can also burn at a lower temperature and with less oxygen than fires on Earth. As a result, the material used to put out space fires must be more concentrated",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Health -10, Hapiness -10, Spacecraft Health -10
-  //   // Solution: Let it die out: Health -10, Hapiness -10, Spacecraft Health -10
-  //   // Solution: Use water: Hapiness -2, Spacecraft Health -1, Water -20
-  //   // Solution: Use fire extiguisher: Fire estinguisher -1
-  // },
-  // {
-  //   name: "Sick",
-  //   content: "Even though all astronauts were in quarantine before the flight, it seems one of them has caught the flue.",
-  //   chance: 0.01,
-  //   chanceIncrement: 0,
-  //   // Effect: Health -10, Hapiness -10
-  //   // Solution: It's just the flue: Health -30, Hapiness -7
-  //   // Solution: Isolate him: Health -10, Hapiness -10
-  //   // Solution: Isolate him and give medicine: Hapiness -3, Medicine -2
-  //   // Solution: Give everybody medicine: Hapiness -2, Medicine -10
-  // }
+  {
+    name: "I like this song",
+    content: "One of your crew was playing a song that you like. Sharing this made the bond between you stronger.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { hapiness: 5 },
+    solutions: [
+      { text: 'I like this song too', effects: { hapiness: 5 } },
+    ]
+
+    // Effect: Hapiness +5
+  },
+  {
+    name: "Can you please turn that down?",
+    content: "One of your crew was playing a song that you hate on repeat. You told them to skip the song, but they ignored you and turned the volume up",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { hapiness: -5 },
+    solutions: [
+      { text: 'Yell at them to turn it down', effects: { hapiness: -5 } },
+      { text: 'Play your own music louder', effects: { hapiness: -4 } },
+      { text: 'Explain why you can\'t stand the song', effects: { hapiness: -3 } },
+    ]
+    // Effect: Hapiness -5
+    // Solution: Yell at them to turn it down: Hapiness -5
+    // Solution: Play your own music louder: Hapiness -4, Music -2
+  },
+  {
+    name: "Water leak",
+    content: "One of the water pipes burst and you have a leak in the crew module.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { aircraft: -10, hapiness: -5, water: -30 },
+    solutions: [
+      { text: 'Close the valve', effects: { aircraft: -10, hapiness: -5, water: -30 } },
+      { text: 'Replace the pipes', effects: { water: -20, equipment: -20 } },
+    ]
+    // Effect: Spacecraft Health -10, Hapiness -5, Water -30
+    // Solution: Close the valve: Spacecraft Health -10, Hapiness -5, Water -30
+    // Solution: Replace the pipes: Water -20, Spare parts -20
+  },
+  {
+    name: "Water or Ice",
+    content: "Water in the main tank has frozen due to an insulator that fell of during launch.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { aircraft: -10, hapiness: -5, water: -30 },
+    solutions: [
+      { text: 'Ignore it, we still have a reserve', effects: { aircraft: -10, hapiness: -5, water: -30 } },
+      { text: 'Create a heater and melt the ice', effects: { equipment: -10 } },
+    ]
+    // Effect: Spacecraft Health -10, Hapiness -5, Water -30
+    // Solution: Ignore it, we still have a reserve: Spacecraft Health -10, Hapiness -5, Water -30
+    // Solution: Create a heater and melt the ice: Spare parts -10
+  },
+  {
+    name: "Movie night",
+    content: "One of the astronauts proposes you do a movie night.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { hapiness: 5 },
+    solutions: [
+      { text: 'We have work to do', effects: { hapiness: -2 } },
+      { text: 'Movie night', effects: { hapiness: 5 } },
+      { text: 'Bring out the pizza', effects: { hapiness: +10, food: -10 } },
+    ]
+    // Effect: Hapiness +5, Movies -2
+    // Solution: We have work to do: Hapiness -2
+    // Solution: Movie night: Hapiness +5, Movies -2
+    // Solution: Bring out the pizza: Hapiness +10, Movies -2, Food -10
+  },
+  {
+    name: "Green or Gray",
+    //https://99designs.com/blog/tips/how-color-impacts-emotions-and-behaviors/
+    content: "Seeing green plants and taking care of them can be a great mood booster. The color green can make you feel optimistic and refreshed. When everything around is gray the mood of the crew will be more bland.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { hapiness: 5, water: -5 },
+    solutions: [
+      { text: 'Let\'s plant some plants', effects: { hapiness: 5, water: -5 } },
+      { text: ' We can\'t waste the water', effects: { hapiness: -5 } },
+    ]
+    // Effect: Hapiness +5, Movies -2
+    // Solution: Let's plant some plants: Hapiness +5, Water -5
+    // Solution: We can't waste the water: Hapiness -5
+  },
+  {
+    name: "Fire",
+    // https://www.space.com/13766-international-space-station-flex-fire-research.html
+    content: "Fire is a different beast in space than it is on the ground. \nWhen flames burn on Earth, heated gases rise from the fire, drawing oxygen in and pushing combustion products out. In microgravity, hot gases don't rise. So an entirely different process, called molecular diffusion, drives flame behavior.\n" +
+      "Space flames can also burn at a lower temperature and with less oxygen than fires on Earth. As a result, the material used to put out space fires must be more concentrated",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { health: -10, hapiness: -10, aircraft: -10 },
+    solutions: [
+      { text: 'Let it die out', effects: { health: -10, hapiness: -10, aircraft: -10 } },
+      { text: 'Use watert', effects: { health: -2, aircraft: -1, water: -20 } },
+      // TODO: { text: 'Use fire extiguisher', effects: { Fire estinguisher -1 } },
+    ]
+    // Effect: Health -10, Hapiness -10, Spacecraft Health -10
+    // Solution: Let it die out: Health -10, Hapiness -10, Spacecraft Health -10
+    // Solution: Use water: Hapiness -2, Spacecraft Health -1, Water -20
+    // Solution: Use fire extiguisher: Fire estinguisher -1
+  },
+  {
+    name: "Sick",
+    content: "Even though all astronauts were in quarantine before the flight, it seems one of them has caught the flue.",
+    chance: 0.01,
+    chanceIncrement: 0,
+    effects: { health: -10, hapiness: -10 },
+    solutions: [
+      { text: 'It\'s just the flue', effects: { health: -30, hapiness: -7 } },
+      { text: 'Isolate him', effects: { health: -10, hapiness: -10 } },
+      { text: 'Isolate him and give medicine', effects: { health: -3, medicine: -2 } },
+      { text: 'Give everybody medicine', effects: { health: -2, medicine: -10 } },
+    ]
+    // Effect: Health -10, Hapiness -10
+    // Solution: It's just the flue: Health -30, Hapiness -7
+    // Solution: Isolate him: Health -10, Hapiness -10
+    // Solution: Isolate him and give medicine: Hapiness -3, Medicine -2
+    // Solution: Give everybody medicine: Hapiness -2, Medicine -10
+  }
 
 
 ];
